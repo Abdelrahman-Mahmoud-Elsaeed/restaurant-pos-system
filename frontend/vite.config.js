64053b6495f1d480ed6986e27590938e2658b9eb
Abdelrahman-Mahmoud-Elsaeed <abdo567465@gmail.com>
2026-04-25 16:17:31 +0300

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,27 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  resolve : {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@api": path.resolve(__dirname, "src/api"),
+      "@app": path.resolve(__dirname, "src/app"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@services": path.resolve(__dirname, "src/services"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@components": path.resolve(__dirname, "src/component"),
+      "@layouts": path.resolve(__dirname, "src/component/layouts"),
+      "@ui": path.resolve(__dirname, "src/component/ui"),
+    }
+  }
 })
