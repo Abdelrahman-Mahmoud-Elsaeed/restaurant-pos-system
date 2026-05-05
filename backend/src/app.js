8dae8462c6c4b573cedc200d3afd74090cf743a0
maikhaledaboeldahab <maikhaled.aboeldahab@gmail.com>
2026-05-05 19:06:35 +0300

    added products and orders contrlollers and routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 import { connectDB,disconnectDB } from "./config/db.js";
 import authRoute  from "./routes/auth.route.js";
+import {getAllproducts, CreateProduct ,getproductbyid, UpdateProduct , DeleteProduct} from "./controllers/product.controller.js"
+import {getAllorders,getorderbyid,UpdateOrder,CreateOrder,DeleteOrder} from "./controllers/order.controller.js"
 
 const app = express();
 
@@ -19,5 +21,17 @@ process.on("SIGINT", async () => {
   process.exit(0);
 });
 
+
+app.get('/api/menue/',getAllproducts);
+app.get('/api/menue/:id',getproductbyid);
+app.post('/api/menue/',CreateProduct);
+app.patch('/api/menue/:id',UpdateProduct);
+app.delete('/api/menue/:id',DeleteProduct);
+
+app.get('/api/order/',getAllorders);
+app.get('/api/order/:id',getorderbyid);
+app.post('/api/order/',CreateOrder);
+app.patch('/api/order/:id',UpdateOrder);
+app.delete('/api/order/:id',DeleteOrder);
 
 export default app
--- backend/src/models/product.model.js
+++ backend/src/models/product.model.js
@@ -1,23 +1,22 @@
-import mongoose from "moongose"
-const Product_Schema=new mongoose.Schema({
-  name:{
-    type :String,
-    required : true
+import mongoose from "mongoose";
+const Product_Schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
   },
-  price:{
-    type : Number,
-    required : true
+  price: {
+    type: Number,
+    required: true,
   },
-  stock:{
-    type : Number,
-    required : true
+  stock: {
+    type: Number,
+    required: true,
   },
-  category:{
-    type : String,
-    required : true
-  }
+  category: {
+    type: String,
+    required: true,
+  },
+});
+const model_Product = mongoose.model("Product", Product_Schema);
 
-})
-const model_Product=mongoose.model("Product" , Product_Schema)
-
-export default Product_Schema
+export default model_Product;
