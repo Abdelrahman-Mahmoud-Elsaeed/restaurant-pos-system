8bd5a3a99d8e5152d0c8c4be6c89c9ecfcbffb81
Abdelrahman-Mahmoud-Elsaeed <abdo567465@gmail.com>
2026-05-16 13:37:33 +0300

    update

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,26 +1,23 @@
-import Navbar from "../component/layouts/Navbar/Navbar";
-import OverlaySidebar from "../component/layouts/OverlaySidebar/OverlaySidebar.jsx";
-import Sidebar from "../component/layouts/Sidebar/Sidebar";
-import Button from "../component/ui/Button/Button";
-import Taps from "../component/ui/Taps/Taps";
-import KitchenPos from "../pages/KitchenPos/KitchenPos.jsx";
-
-import Login from "../pages/login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainLayout from "./MainLayout";
+import Login from "../pages/Loign/login";
+import KitchenPos from "../pages/KitchenPos/KitchenPos";
 
 function App() {
   return (
-    <>
-      <div className="d-flex min-100vh" >
-        <div>
-          <Sidebar></Sidebar>
-        </div>
-        <div className="w-100" >
-          <Navbar></Navbar>
-          <KitchenPos/>
-          <OverlaySidebar></OverlaySidebar>
-        </div>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<KitchenPos />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/inventory" element={<Inventory />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/settings" element={<Settings />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
